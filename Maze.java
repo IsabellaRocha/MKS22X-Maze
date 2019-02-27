244df48e62768b8e443931ee0bc44fd711c73179
Isabella Rocha <irocha00@stuy.edu>
2019-02-26 23:09:03 -0500

    Added method to make it a 2d array

--- Maze.java
+++ Maze.java
@@ -10,6 +10,27 @@ public class Maze {
     }
     return output;
   }
+  public static char[][] readFileArray(String x) throws FileNotFoundException{
+    File text = new File(x);
+    Scanner Maze = new Scanner(text);
+    int r = 0;
+    int c = 0;
+    while (Maze.hasNextLine()) {
+      r++;
+      String line = Maze.nextLine();
+      c = line.length();
+    }
+    char[][] output = new char[r][c];
+    Maze = new Scanner(text);
+    while (Maze.hasNextLine()) {
+      int i = 0;
+      for (int idx = 0; idx < Maze.nextLine().length(); idx++) {
+        output[i][idx] = Maze.nextLine().charAt(idx);
+      }
+      i++;
+    }
+    return output;
+  }
   public static void main(String args[]) {
     try {
       System.out.println(readFile("Maze1.txt"));
